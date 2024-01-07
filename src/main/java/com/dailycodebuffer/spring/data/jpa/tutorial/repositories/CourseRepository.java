b8cb2944ace61d21d60e8237bc687648a0ffa735
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-01-07 13:31:25 -0300

    Paging and Sorting

--- src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/repositories/CourseRepository.java
+++ src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/repositories/CourseRepository.java
@@ -1,9 +1,15 @@
 package com.dailycodebuffer.spring.data.jpa.tutorial.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.dailycodebuffer.spring.data.jpa.tutorial.entities.Course;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long>{
+
+    Page<Course> findByTitleContaining(String title, Pageable pageable);
+
 }
--- src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repositories/CourseRepositoryTest.java
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repositories/CourseRepositoryTest.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.dailycodebuffer.spring.data.jpa.tutorial.entities.Course;
 import com.dailycodebuffer.spring.data.jpa.tutorial.entities.Teacher;
@@ -35,6 +38,46 @@ public class CourseRepositoryTest {
             .build();
 
         repository.save(course);
+    }
+
+    @Test
+    public void findAllPagination(){
+        Pageable firstPageWithThreeRecords = PageRequest.of(0, 3);
+        Pageable secondPageWithTwoRecords = PageRequest.of(1, 2);
+
+        List<Course> courses = repository.findAll(firstPageWithThreeRecords).getContent();
+
+        long totalElements = repository.findAll(firstPageWithThreeRecords).getTotalElements();
+
+        long totalPages = repository.findAll(firstPageWithThreeRecords).getTotalPages();
+
+        System.out.println("totalElements = " + totalElements);
+
+        System.out.println("totalPages = " + totalPages);
+
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void findAllSorting(){
+        Pageable sortByTitle = PageRequest.of(0, 2, Sort.by("title"));
+
+        Pageable sortByCreditDesc = PageRequest.of(0, 2, Sort.by("credit").descending());
+
+        Pageable sortByTitleAndCreditDesc = PageRequest.of(0, 2, Sort.by("title").descending().and(Sort.by("credit")));
+
+        List<Course> courses = repository.findAll(sortByTitle).getContent();
+
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void printFindByTitleContaining(){
+        Pageable firstPageTenRecords = PageRequest.of(0, 10);
+
+        List<Course> courses = repository.findByTitleContaining("D", firstPageTenRecords).getContent();
+
+        System.out.println("courses = " + courses);
     }
 
 }
