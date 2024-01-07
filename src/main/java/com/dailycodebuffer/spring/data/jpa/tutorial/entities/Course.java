ca298c2902935b176b402c0ef957553bc0c79397
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-01-06 22:24:40 -0300

    Uni & Bi directional Relationship

--- src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entities/Course.java
+++ src/main/java/com/dailycodebuffer/spring/data/jpa/tutorial/entities/Course.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,4 +24,8 @@ public class Course {
     private String title;
     private Integer credit;
 
+    @OneToOne(
+        mappedBy = "course"
+    )
+    private CourseMaterial courseMaterial;
 }
